1dbdf3322d7bfafae18d98bb60d644ca96d6e45b
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-09-13 21:33:00 +0500

    Added two more rows of a different kind of cars and set them animated

--- app.js
+++ app.js
@@ -32,32 +32,61 @@ const frogAspectRatio = frogHeight / frogWidth
 const frogShowWidth = 20 / frogAspectRatio
 const frogShowHeight = 20
 
-const carX = 408
-const carY = 175
-const carWidth = 490 - carX
-const carHeight = 220 - carY
+const car1X = 408
+const car1Y = 175
+const car1Width = 490 - car1X
+const car1Height = 220 - car1Y
 
-const carAspectRatio = carHeight / carWidth
-const carShowWidth = 40 / carAspectRatio
-const carShowHeight = 40
+const car1AspectRatio = car1Height / car1Width
+const car1ShowWidth = 40 / car1AspectRatio
+const car1ShowHeight = 40
 
-const spacing = Math.floor((CANWIDTH - 2 * carShowWidth) / 2)
+const spacing1 = Math.floor((CANWIDTH - 2 * car1ShowWidth) / 2)
 
-const speed = 1
+const speed1 = 1
+
+const car2X = 440
+const car2Y = 65
+const car2Width = 529 - car2X
+const car2Height = 107 - car2Y
+
+const car2AspectRatio = car2Height / car2Width
+const car2ShowWidth = 40 / car2AspectRatio
+const car2ShowHeight = 40
+
+const spacing2 = Math.floor((CANWIDTH - 2 * car2ShowWidth) / 2)
+
+const speed2 = 1.5
 
 let row1 = []
 for (let i = 0 ; i < 2 ; i++) {
-    row1[i] = {x: i * spacing + i * carShowWidth, y: 580 - carShowHeight}
+    row1[i] = {x: i * spacing1 + i * car1ShowWidth, y: 580 - car1ShowHeight}
 }
 
 let row2 = []
 for (let i = 0 ; i < 2 ; i++) {
-    row2[i] = {x: i * spacing + (i + 1) * carShowWidth, y: 580 - carShowHeight}
+    row2[i] = {x: i * spacing1 + (i + 1) * car1ShowWidth, y: 580 - car1ShowHeight}
 }
 
+let row3 = []
+for (let i = 0 ; i < 2 ; i++) {
+    row3[i] = {x: i * spacing2 + i * car2ShowWidth, y: 500 - car2ShowHeight}
+}
+
+let row4 = []
+for (let i = 0 ; i < 2 ; i++) {
+    row4[i] = {x: i * spacing2 + (i + 1) * car2ShowWidth, y: 500 - car2ShowHeight}
+}
+
+console.log(car2ShowWidth, car2Width)
+console.log(row3)
+console.log(row4)
+
+console.log(row1, row2)
+
 let car1 = {
-    x: 800 - carShowWidth,
-    y: 580 - carShowHeight,
+    x: 800 - car1ShowWidth,
+    y: 580 - car1ShowHeight,
     speed: 1
 }
 
@@ -67,8 +96,8 @@ let car1Shadow = {
 }
 
 let car2 = {
-    x: carShowWidth,
-    y: 580 - carShowHeight,
+    x: car1ShowWidth,
+    y: 580 - car1ShowHeight,
     speed: 1
 }
 
@@ -81,9 +110,9 @@ function animate2() {
     
     let column = frame % 3
     ctx2.drawImage(img2, frogX, frogY, frogWidth, frogHeight, frogLocation.x, frogLocation.y, frogShowWidth, frogShowHeight)
-    //ctx2.drawImage(img1, carX, carY, carWidth, carHeight, car1.x, car1.y, carShowWidth, carShowHeight)
-    //ctx2.drawImage(img1, carX, carY, carWidth, carHeight, car1Shadow.x, car1Shadow.y, carShowWidth, carShowHeight)
-    row1.forEach( car => ctx2.drawImage(img1, carX, carY, carWidth, carHeight, car.x, car.y, carShowWidth, carShowHeight))
+    //ctx2.drawImage(img1, car1X, car1Y, carWidth, carHeight, car1.x, car1.y, carShowWidth, carShowHeight)
+    //ctx2.drawImage(img1, car1X, car1Y, carWidth, carHeight, car1Shadow.x, car1Shadow.y, carShowWidth, carShowHeight)
+    row1.forEach( car => ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, car.x, car.y, car1ShowWidth, car1ShowHeight))
     
     
     /*if (car1.x < -carShowWidth) {
@@ -96,21 +125,43 @@ function animate2() {
 
     ctx2.rotate(Math.PI)
     //ctx2.drawImage(img1, carX, carY, carWidth, carHeight, -car2.x, -car2.y, carShowWidth, carShowHeight)
-    row2.forEach( car => ctx2.drawImage(img1, carX, carY, carWidth, carHeight, -car.x, -car.y, carShowWidth, carShowHeight))
+    row2.forEach( car => ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, -car.x, -car.y, car1ShowWidth, car1ShowHeight))
     ctx2.restore()
 
     row1 = row1.map(car => {
         return {
-            x: car.x - speed < -carShowWidth ? CANWIDTH : car.x - speed, 
+            x: car.x - speed1 < -car1ShowWidth ? CANWIDTH : car.x - speed1, 
             y: car.y
         }
     })
     row2 = row2.map(car => {
         return {
-            x: car.x + speed - carShowWidth > CANWIDTH ? 0 : car.x + speed, 
+            x: car.x + speed1 - car1ShowWidth > CANWIDTH ? 0 : car.x + speed1, 
             y: car.y
         }
     })
+
+    row3.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, car.x, car.y, car2ShowWidth, car2ShowHeight))
+        ctx2.save()
+
+    ctx2.rotate(Math.PI)
+    //ctx2.drawImage(img1, carX, carY, carWidth, carHeight, -car2.x, -car2.y, carShowWidth, carShowHeight)
+    row4.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, -car.x, -car.y, car2ShowWidth, car2ShowHeight))
+    ctx2.restore()
+
+    row3 = row3.map(car => {
+        return {
+            x: car.x - speed2 < -car2ShowWidth ? CANWIDTH : car.x - speed2, 
+            y: car.y
+        }
+    })
+    row4 = row4.map(car => {
+        return {
+            x: car.x + speed2 - car2ShowWidth > CANWIDTH ? 0 : car.x + speed2, 
+            y: car.y
+        }
+    })
+
     requestAnimationFrame(animate2)
     
 }

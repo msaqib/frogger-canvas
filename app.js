59da718c4061b91e8435ad4990f854ff90ded747
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-09-17 19:17:43 +0500

    All cars and trucks moving

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cvs2 = document.getElementById('frogger-canvas')
 const ctx2 = cvs2.getContext('2d')
 
 const CANWIDTH = cvs2.width = 800
-const CANHEIGHT = cvs2.height = 600
+const CANHEIGHT = cvs2.height = 540
 
 const img1 = new Image()
 img1.src = 'assets/26755.png'
@@ -12,7 +12,7 @@ img2.src = 'assets/frogger.png'
 
 let frogLocation = {
     x: 400,
-    y: 580
+    y: 520
 }
 
 let scale2 = 1
@@ -58,9 +58,22 @@ const spacing2 = Math.floor((CANWIDTH - 2 * car2ShowWidth) / 2)
 
 const speed2 = 1.5
 
+const truckX = 8
+const truckY = 230
+const truckWidth = 97 - truckX
+const truckHeight = 275 - truckY
+
+const truckAspectRatio = truckHeight / truckWidth
+const truckShowWidth = 40 / truckAspectRatio
+const truckShowHeight = 40
+
+const spacing3 = Math.floor((CANWIDTH - 2 * truckShowWidth) / 2)
+
+const speed3 = 2
+
 let row1 = []
 for (let i = 0 ; i < 2 ; i++) {
-    row1[i] = {x: CANWIDTH - (i * spacing1 + (i + 1) * car1ShowWidth), y: 580 - car1ShowHeight}
+    row1[i] = {x: CANWIDTH - (i * spacing1 + (i + 1) * car1ShowWidth), y: CANHEIGHT - 20 - car1ShowHeight}
 }
 
 let row2 = []
@@ -70,14 +83,23 @@ for (let i = 0 ; i < 2 ; i++) {
 
 let row3 = []
 for (let i = 0 ; i < 2 ; i++) {
-    row3[i] = {x: i * spacing2 + i * car2ShowWidth, y: 500 - car2ShowHeight}
+    row3[i] = {x: i * spacing2 + i * car2ShowWidth, y: CANHEIGHT - 20 - 2 * car1ShowHeight - car2ShowHeight}
 }
 
 let row4 = []
 for (let i = 0 ; i < 2 ; i++) {
-    row4[i] = {x: i * spacing2 + (i + 1) * car2ShowWidth, y: 500 - car2ShowHeight}
+    row4[i] = {x: i * spacing2 + (i + 1) * car2ShowWidth, y: 20 + car2ShowHeight + 2*car1ShowHeight}
 }
 
+let row5 = []
+for (let i = 0 ; i < 2 ; i++) {
+    row5[i] = {x: i * spacing3 + i * truckShowWidth, y: CANHEIGHT - 20 - 2*car1ShowHeight - 2*car2ShowHeight - truckShowHeight}
+}
+
+let row6 = []
+for (let i = 0 ; i < 2 ; i++) {
+    row6[i] = {x: i * spacing3 + i * truckShowWidth, y: 20 + truckShowHeight + 2*car1ShowHeight + 2*car2ShowHeight}
+}
 
 let car1 = {
     x: CANWIDTH - car1ShowWidth,
@@ -99,7 +121,6 @@ let car2 = {
 function animate2() {
     ctx2.clearRect(0, 0, CANWIDTH, CANHEIGHT)
     
-    let column = frame % 3
     ctx2.drawImage(img2, frogX, frogY, frogWidth, frogHeight, frogLocation.x, frogLocation.y, frogShowWidth, frogShowHeight)
     row1.forEach( car => ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, car.x, car.y, car1ShowWidth, car1ShowHeight))
     
@@ -110,35 +131,35 @@ function animate2() {
     row2.forEach( car => ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, car.x, car.y, car1ShowWidth, car1ShowHeight))
     ctx2.restore()
 
-    row1 = row1.map((car) => checkAndReset(car, speed1, car1ShowWidth))
-    row2 = row2.map((car) => checkAndReset(car, speed1, car1ShowWidth))
+    row1 = row1.map((car) => incrementCheckAndReset(car, speed1, car1ShowWidth))
+    row2 = row2.map((car) => incrementCheckAndReset(car, speed1, car1ShowWidth))
 
-    // row3.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, car.x, car.y, car2ShowWidth, car2ShowHeight))
-    //     ctx2.save()
+    row3.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, car.x, car.y, car2ShowWidth, car2ShowHeight))
+    ctx2.save()
 
-    // ctx2.rotate(Math.PI)
-    // ctx2.translate(-CANWIDTH, -CANHEIGHT)
-    // row4.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, -car.x, -car.y, car2ShowWidth, car2ShowHeight))
-    // ctx2.restore()
+    ctx2.rotate(Math.PI)
+    ctx2.translate(-CANWIDTH, -CANHEIGHT)
+    row4.forEach( car => ctx2.drawImage(img1, car2X, car2Y, car2Width, car2Height, car.x, car.y, car2ShowWidth, car2ShowHeight))
+    ctx2.restore()
 
-    // row3 = row3.map(car => {
-    //     return {
-    //         x: car.x - speed2 < -car2ShowWidth ? CANWIDTH : car.x - speed2, 
-    //         y: car.y
-    //     }
-    // })
-    // row4 = row4.map(car => {
-    //     return {
-    //         x: car.x + speed2 - car2ShowWidth > CANWIDTH ? 0 : car.x + speed2, 
-    //         y: car.y
-    //     }
-    // })
+    row3 = row3.map((car) => incrementCheckAndReset(car, speed2, car2ShowWidth))
+    row4 = row4.map((car) => incrementCheckAndReset(car, speed2, car2ShowWidth))
 
+    row5.forEach( truck => ctx2.drawImage(img1, truckX, truckY, truckWidth, truckHeight, truck.x, truck.y, truckShowWidth, truckShowHeight))
+    ctx2.save()
+
+    ctx2.rotate(Math.PI)
+    ctx2.translate(-CANWIDTH, -CANHEIGHT)
+    row6.forEach( truck => ctx2.drawImage(img1, truckX, truckY, truckWidth, truckHeight, truck.x, truck.y, truckShowWidth, truckShowHeight))
+    ctx2.restore()
+
+    row5 = row5.map((truck) => incrementCheckAndReset(truck, speed3, truckShowWidth))
+    row6 = row6.map((truck) => incrementCheckAndReset(truck, speed3, truckShowWidth))
     requestAnimationFrame(animate2)
     
 }
 
-function checkAndReset(vehicle, speed, showWidth) {
+function incrementCheckAndReset(vehicle, speed, showWidth) {
     return {
         x: vehicle.x - speed < -showWidth ? CANWIDTH : vehicle.x - speed,
         y: vehicle.y
@@ -146,25 +167,3 @@ function checkAndReset(vehicle, speed, showWidth) {
 }
 
 animate2()
-
-function animate3() {
-    //car1.x = car1.x - car1.speed
-    //car1Shadow.x = car1Shadow.x - car1.speed
-    //car2.x = car2.x + car2.speed
-    ctx2.clearRect(0, 0, CANWIDTH, CANHEIGHT)
-    car1.x -= speed1
-    if (car1.x < -car1ShowWidth) {
-        car1.x = CANWIDTH
-    }
-    let column = frame % 3
-    ctx2.drawImage(img2, frogX, frogY, frogWidth, frogHeight, frogLocation.x, frogLocation.y, frogShowWidth, frogShowHeight)
-    ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, car1.x, car1.y, car1ShowWidth, car1ShowHeight)
-    ctx2.save()
-    ctx2.rotate(Math.PI)
-    ctx2.translate(-CANWIDTH, -CANHEIGHT)
-    ctx2.drawImage(img1, car1X, car1Y, car1Width, car1Height, car2.x, car2.y, car1ShowWidth, car1ShowHeight)
-    ctx2.restore()
-    requestAnimationFrame(animate3)
-}
-
-//animate3()
